4f4c4e8f620a65df75d6b27ae7de5d95e6cdd823
Oláh <adam.olah06@gmail.com>
2026-03-12 19:49:08 +0100

    New export file extension imports added

--- weatherCrossing_utils/processes/loggers/__init__.py
+++ weatherCrossing_utils/processes/loggers/__init__.py
@@ -1,5 +1,7 @@
+from .CSV_log_file import *
 from .JSON_log_file import *
 from .PDF_log_file import *
+from .XLSX_log_file import *
 from ...classes.ConsolColor import *
 
 print(ConsolColor.PreSetUpColoredTextLine(f"Logger loaded".upper(), "success"))
