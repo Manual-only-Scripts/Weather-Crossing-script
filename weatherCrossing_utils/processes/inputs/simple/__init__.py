f412237c51511d8a20d57807c12bcc936dca2dba
Oláh <adam.olah06@gmail.com>
2026-03-22 01:15:01 +0100

    import updated

--- weatherCrossing_utils/processes/inputs/simple/__init__.py
+++ weatherCrossing_utils/processes/inputs/simple/__init__.py
@@ -1,6 +1,7 @@
 from .Ask_Coordinate import *
 from .Ask_Date import *
 from .Ask_UnitGroup import *
+from .Ask_Name import *
 from ....classes.ConsolColor import *
 
 print(ConsolColor.PreSetUpColoredTextLine(f"Simple asks loaded".upper(), "success"))
